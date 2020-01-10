40e0a876f07c6e5f0b0d2d3d9d0cb59da93f3c86
Georg Schmidt <webgsch1@web.de>
2020-01-10 17:53:22 +0100

    Added refresh functionality, misc improvements

--- popup.js
+++ popup.js
@@ -1,20 +1,39 @@
+// Listen for changes to storage.sync
 chrome.storage.onChanged.addListener(function(){
 
-    let watchList = document.getElementById('watchList');
-    chrome.storage.sync.get('watchlist', function(watchlist){
+    displayWatchList();
+});
 
-        let articleWatchlist;
-        articleWatchlist = watchlist.watchlist;
+// Listen for click event on refresh button of watchList
+let refreshWatchList = document.getElementById('refreshWatchList');
+
+refreshWatchList.addEventListener("click", () => {
+
+    displayWatchList();
+});
+
+function displayWatchList(){
+
+    // Assign HTML wrapper of watchlist to variable
+    let watchListWrapper = document.getElementById('watchListWrapper');
+
+    // Get content from storage.sync
+    chrome.storage.sync.get('watchList', function(result){
+
+        let articleWatchlist = result.watchList;
+
+        // Clear current list of saved articles then write new list entries
+        watchListWrapper.innerHTML = '';
+
         articleWatchlist.forEach(function(article){
 
-            console.log(article[0]);
-            debugger;
-            
-            watchList.insertAdjacentHTML('afterbegin', `
-                <li>
-                    <a href="${article[1]}">${article[0]}</a>
-                </li>
-            `);
+        console.log(articleWatchlist);
+        
+        watchListWrapper.insertAdjacentHTML('afterbegin', `
+            <li>
+                <a href="${article.url}" target="_blank" rel="noopener">${article.title}</a>
+            </li>
+        `);
         });
     });
-})
+}
